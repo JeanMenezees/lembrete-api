bd0a04431fff71165d2d8b10460c68c4faeaa8d6
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-07-21 19:50:44 -0300

    fix: corrigir erros de build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuarioService } from './aplicacao/usuario/usuario.service';
-import { LembreteService } from './aplicacao/lembrete/lembrete.service';
-import { UsuarioController } from './api/usuario/usuario.controller';
-import { LembreteController } from './api/lembrete/lembrete.controller';
 import { UsuarioModule } from './modules/usuario/usuario.module';
 import { LembreteModule } from './modules/lembrete/lembrete.module';
 
 @Module({
   imports: [UsuarioModule, LembreteModule],
-  controllers: [AppController, UsuarioController, LembreteController],
-  providers: [AppService, UsuarioService, LembreteService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
