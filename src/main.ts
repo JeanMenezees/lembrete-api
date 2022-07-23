75866c054124f2e1f30b52aec03fc3e368706642
Jean Menezes <jeanvbonimenezes@gmail.com>
2022-07-23 01:47:42 -0300

    feat: adicionar logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
 
   app.setGlobalPrefix('/api/v1');
 
